28431f8d8da53a67e87851a1882e1d574e301f67
sxyxxn <wjdthdus5@gmail.com>
2022-09-27 10:55:31 +0900

    반복 구문 연습

--- ifelse.py
+++ ifelse.py
@@ -1,13 +1,35 @@
 # ifelse.py
-score = int(input("점수를 입력:"))
+# 선택한 영역 주석처리 : ctrl + /
+# score = int(input("점수를 입력:"))
 
-if 90 <= score <= 100:
-    grade = "A"
-elif 80 <= score < 90:
-    grade = "B"
-elif 70 <= score < 80:
-    grade = "C"
-else:
-    grade = "D"
+# if 90 <= score <= 100:
+#     grade = "A"
+# elif 80 <= score < 90:
+#     grade = "B"
+# elif 70 <= score < 80:
+#     grade = "C"
+# else:
+#     grade = "D"
 
-print("등급은 ",grade)
+# print("등급은 ",grade)
+
+value = 5
+while value > 0:
+    print(value)
+    value -= 1
+
+d = {"apple":100, "orange":200}
+for item in d.items():
+    print(item)
+
+lst = [1,2,3,4,5,6,7,8,9,10]
+for item in lst:
+    if item > 5:
+        break
+    print("Item:{0}".format(item))
+
+print("---continue---")
+for item in lst:
+    if item % 2 == 0:
+        continue
+    print("Item:{0}".format(item))
