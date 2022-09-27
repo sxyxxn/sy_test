f8b5617ca4be24d082551b626094c94b60c14a5e
sxyxxn <wjdthdus5@gmail.com>
2022-09-27 14:46:16 +0900

    이름 숨김 연습

--- BankAccount.py
+++ BankAccount.py
@@ -2,19 +2,30 @@
 
 #은행의 계정을 표현한 클래스 
 class BankAccount:
+    # 초기화 메서드
     def __init__(self, id, name, balance):
-        self.id = id
-        self.name = name 
-        self.balance = balance 
+        # 이름 변경(이름 규칙)(__를 붙여서 이름을 숨겨줌)
+        self.__id = id
+        self.__name = name 
+        self.__balance = balance 
+    # 입금
     def deposit(self, amount):
-        self.balance += amount 
+        self.__balance += amount 
+    # 출금
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
+    # 결과 문자열
     def __str__(self):
-        return "{0} , {1} , {2}".format(self.id, \
-            self.name, self.balance)
+        return "{0} , {1} , {2}".format(self.__id, \
+            self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
 account1.withdraw(3000)
 print(account1)
+#print(account1.__balance)
+# 외부에서는 _클래스명__변수명 => 백도어(테스트 용도)
+print(account1._BankAccount__balance)
+ 
+#account1.balance = 15000000
+#print(account1.balance)
--- isinstace함수_파생여부확인.py
+++ isinstace함수_파생여부확인.py
@@ -6,6 +6,7 @@ class Student(Person):
     pass
 p, s = Person(), Student()
 
+#
 print("p is instance of Person: ", isinstance(p, Person))
 print("s is instance of Person: ", isinstance(s, Person))
 print("p is instance of Object: ", isinstance(p, object))
