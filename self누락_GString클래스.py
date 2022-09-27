11a9a6ffb31eb7e49358efe85477c1e24e86f9d5
sxyxxn <wjdthdus5@gmail.com>
2022-09-27 14:00:24 +0900

    클래스 파일 연습2

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,11 +1,14 @@
+# 전역변수
 str = "Not Class Member"
 class GString:
     def __init__(self):
+        # 인스턴스 멤버 변수
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        print(str) # 전역변수로 인식. self.str로 변경해서 명시적 코딩 해야함
+        print(self.str)
 
 g = GString()
 g.set("First Message")
