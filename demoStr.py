457d0d01dacd52044d91ae09ec9257c827dde560
sxyxxn <wjdthdus5@gmail.com>
2022-09-28 11:19:04 +0900

    문자열 형식 메서드 연습1

---
+++ demoStr.py
@@ -0,0 +1,11 @@
+# demoStr.py
+#print( dir(str) )
+
+strA = "python is very powerful"
+strB = "파이썬은 강력해"
+print( len(strA) )
+print( len(strB) )
+print( strA.capitalize() )
+print( strA.upper() )
+print( strA.count("p") )
+print( strA.count("p",7) )
