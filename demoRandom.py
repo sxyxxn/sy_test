9fec4b744639907d378f9eecdd1476ec40c36729
sxyxxn <wjdthdus5@gmail.com>
2022-09-28 14:50:37 +0900

    파일, 폴더, 운영체제 다루기

--- demoRandom.py
+++ demoRandom.py
@@ -24,4 +24,12 @@ print( getsize("c:\\python39\\python.exe") ) # 결과*0.001 KB
 # 운영체제 정보
 from os import *
 print("운영체제:",name)
-system("notepad.exe") # notepad라는 실행파일 실행해라는 의미
+#system("notepad.exe") # notepad라는 실행파일 실행해라는 의미 > 빈 메모장 열림
+
+# 파일리스트
+import glob
+print( glob.glob("c:\\work\\*.py") ) # 리스트로 출력
+result = glob.glob("c:\\work\\*.*")
+for item in result:
+    print(item) # 하나씩 출력
+
