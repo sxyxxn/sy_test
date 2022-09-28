c5d5e205d301e7ce53f9a0d124a68e08ff4a4e2c
sxyxxn <wjdthdus5@gmail.com>
2022-09-28 11:35:03 +0900

    문자열 형식 메서드 연습 2

--- demoStr.py
+++ demoStr.py
@@ -1,6 +1,9 @@
 # demoStr.py
 #print( dir(str) )
 
+from pickletools import read_uint1
+
+
 strA = "python is very powerful"
 strB = "파이썬은 강력해"
 print( len(strA) )
@@ -9,3 +12,32 @@ print( strA.capitalize() )
 print( strA.upper() )
 print( strA.count("p") )
 print( strA.count("p",7) )
+
+print("---반복 문구---")
+names = ["홍길동","전우치","이순신"]
+for name in names:
+        print("안녕하세요 {0}님 멋진 가을입니다.".format(name))
+        print("=" * 40)
+
+print( "demo.ppt".startswith("demo") )
+print( "demo.ppt".endswith("ppt") )
+print( "MBC2580".isalnum() )
+print( "MBC:2580".isalnum() )
+print( "2580".isdecimal() )
+
+print("---문자열 전처리---") # preprocessing
+#u = " spam and ham "
+#result = u.strip(" ")
+u = "<<< spam and ham >>>"
+result = u.strip("<> ") # <,>,빈칸을 없애라
+print(u)
+print(result)
+result = result.replace("spam", "spam egg")
+print(result)
+
+print("---리스트---")
+lst = result.split()
+print(lst)
+
+print("---다시 합치기---")
+print( ":)".join(lst) )
