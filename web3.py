dcf07e32ef14356da8c48a418581f3edf06b4d78
sxyxxn <wjdthdus5@gmail.com>
2022-09-29 11:17:02 +0900

    페이징 처리하고 파일 저장

---
+++ web3.py
@@ -0,0 +1,24 @@
+# web2.py
+# 웹서버에 요청
+from re import L
+import urllib.request
+# 크롤링
+from bs4 import BeautifulSoup
+
+# 파일에 저장
+f = open("c:\\work\\webtoon.txt","wt",encoding="utf-8")
+
+# 페이징 처리(한페이지에 10개)
+for i in range(1,6):
+    url = "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
+    print(url)
+    data = urllib.request.urlopen(url)
+    soup = BeautifulSoup(data, "html.parser")
+    cartoons = soup.find_all("td", class_="title")
+
+    for item in cartoons:
+        title = item.find("a").text.strip()
+        print(title)
+        f.write(title+"\n")
+        
+f.close()
