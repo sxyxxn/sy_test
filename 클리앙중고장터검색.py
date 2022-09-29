3763cfb7d766586e030792a868c7402721a42a71
sxyxxn <wjdthdus5@gmail.com>
2022-09-29 13:18:37 +0900

    클리앙 파일로 저장

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -7,6 +7,7 @@ import re
 #User-Agent를 조작하는 경우(아이폰에서   사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
+f = open("c:\\work\\clien.txt","wt",encoding="utf-8")
 for n in range(0,10):
         #클리앙의 중고장터 주소 
         data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
@@ -27,14 +28,16 @@ for n in range(0,10):
         for item in list:
                 try:
                         title = item.text.strip()
-                        print(title)
+                        # print(title)
                         # #<a class='list_subject'><span>text</span><span>text</span>
                         # span = item.contents[1]
                         # span2 = span.nextSibling.nextSibling #옆에 있는 값
                         # title = span2.text 
-                        # if (re.search('아이폰', title)):
-                        #         print(title.strip())
+                        if (re.search('맥북', title)):
+                                print(title.strip())
+                                f.write(title.strip() + "\n")
                         
                 except:
                         pass
+f.close()
         
